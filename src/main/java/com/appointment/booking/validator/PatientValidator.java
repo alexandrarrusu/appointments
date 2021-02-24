0206af19885585ebd90da123bd5e855268e6715a
Alexandra Rusu <alexandra@Alexandras-MacBook-Pro.local>
2021-02-24 15:44:55 +0100

    added patient validation

--- src/main/java/com/appointment/booking/validator/PatientValidator.java
+++ src/main/java/com/appointment/booking/validator/PatientValidator.java
@@ -4,12 +4,30 @@ import com.appointment.booking.entity.Patient;
 import com.appointment.booking.exception.BadRequestException;
 import com.appointment.booking.repository.PatientRepository;
 
+import static java.util.Objects.isNull;
+
 public class PatientValidator {
 
     public void validatePatient(Patient patient, PatientRepository patientRepository) {
         String email = patient.getEmail();
-        if(!patientRepository.checkEmailIfExists(email).getEmail().isEmpty()) {
+        String phone = patient.getPhone();
+        validateEmail(email, patientRepository);
+        validatePhone(phone);
+    }
+
+    private void validateEmail(String email, PatientRepository patientRepository) {
+        if(email.length() == 0) {
+            throw new BadRequestException("Email cannot be empty");
+        }
+
+        if(!isNull(patientRepository.checkEmailIfExists(email))) {
             throw new BadRequestException("Email already exists");
+        }
+    }
+
+    private void validatePhone(String phone) {
+        if(phone.length() == 0) {
+            throw new BadRequestException("Phone cannot be null");
         }
     }
 
